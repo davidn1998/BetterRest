5b4d6a1f4d8f0077f5204b2201a39b134e5e51bd
David Nwachukwu <davidnw1998@gmail.com>
2024-06-18 20:49:43 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by David Nwachukwu on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
